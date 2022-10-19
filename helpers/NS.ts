33d6d2bab62ea63cd60a0951beab32cc0cb6dbf0
Wisse <wissehes@gmail.com>
2022-10-19 14:27:54 +0200

    move the api key to .env.local 😟

--- helpers/NS.ts
+++ helpers/NS.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const NS = axios.create({
   baseURL: "https://gateway.apiportal.ns.nl",
   headers: {
-    "Ocp-Apim-Subscription-Key": "ebe8f1f1ad584f309cf6eee7f28bf8c9",
+    "Ocp-Apim-Subscription-Key": process.env.NS_API,
   },
 });
 
