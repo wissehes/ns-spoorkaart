87cd51f328ce871d03a26f9b543d292d1d94f14c
Wisse <wissehes@gmail.com>
2022-10-19 14:29:20 +0200

    Use the NS axios instance

--- helpers/getTrains.ts
+++ helpers/getTrains.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
 import { getTrainsResponse } from "../types/getTrainsResponse";
+import NS from "./NS";
 
 export default async function getTrains() {
-  const { data } = await axios.get<getTrainsResponse>(
-    "https://gateway.apiportal.ns.nl/virtual-train-api/api/vehicle",
-    {
-      headers: {
-        "Ocp-Apim-Subscription-Key": "ebe8f1f1ad584f309cf6eee7f28bf8c9",
-      },
-    }
+  const { data } = await NS.get<getTrainsResponse>(
+    "/virtual-train-api/api/vehicle"
   );
 
   return data;
