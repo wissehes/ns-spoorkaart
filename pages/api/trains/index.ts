d4651eb6a6819a0969d348cc538aa022e828b5b2
Wisse <wissehes@gmail.com>
2022-10-19 16:13:36 +0200

    Move trains api endpoint

--- pages/api/trains/index.ts
+++ pages/api/trains/index.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
-import getTrains from "../../helpers/getTrains";
-import { getTrainsResponse, Trein } from "../../types/getTrainsResponse";
+import getTrains from "../../../helpers/getTrains";
+import { getTrainsResponse, Trein } from "../../../types/getTrainsResponse";
 
 type Data = Trein[];
 
