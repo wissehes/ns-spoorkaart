eeec02c1794ce2d7e85ff177394447195c84e07b
Wisse <wissehes@gmail.com>
2022-10-19 16:15:37 +0200

    Create Station popup component

--- components/StationPopup.tsx
+++ components/StationPopup.tsx
@@ -1,8 +1,10 @@
 import { SmallStation } from "../types/getStationsResponse";
-import styles from "../styles/Map.module.css";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { StationInfoResponse } from "../types/getStationInfoResponse";
+
+import styles from "../styles/Map.module.css";
+import formatTime from "../helpers/formatTime";
 
 export default function StationPopup({ station }: { station: SmallStation }) {
   const query = useQuery(["station", station.code], async () => {
@@ -19,15 +21,33 @@ export default function StationPopup({ station }: { station: SmallStation }) {
         Sporen: <b>{station.sporen.map((s) => s.spoorNummer).join(", ")}</b>
       </p>
 
-      {query.data &&
-        query.data.departures.map((a) => (
-          <div key={a.product.number} style={{ margin: "0px" }}>
-            <p>
-              <b>{a.product.shortCategoryName}</b> naar <b>{a.direction}</b> om{" "}
-              {a.actualDateTime}
-            </p>
-          </div>
-        ))}
+      <div>
+        <table>
+          <thead>
+            <tr style={{ textAlign: "center", fontWeight: "bolder" }}>
+              <th>Tijd</th>
+              <th>Type</th>
+              <th>Spoor</th>
+              <th>Bestemming</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {query.data &&
+              query.data.departures.map((a) => (
+                <tr
+                  key={a.product.number}
+                  className={styles.stationtable_content}
+                >
+                  <th>{formatTime(a.actualDateTime)}</th>
+                  <th>{a.product.shortCategoryName}</th>
+                  <th>{a.plannedTrack}</th>
+                  <th>{a.direction}</th>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
