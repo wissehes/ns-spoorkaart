341268b87aff7ca8abdfcfed6f597a24039bf928
Wisse <wissehes@gmail.com>
2022-10-20 22:30:59 +0200

    Add NS domain name to the allowed images list

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["vt.ns-mlab.nl"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
