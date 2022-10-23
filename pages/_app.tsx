5226be0f1dd708ae3b836de1f49942427639c82d
Wisse <wissehes@gmail.com>
2022-10-24 00:36:08 +0200

    Add the progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,22 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Router from "next/router";
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#38a169",
+  className: "bar-of-progress",
+  delay: 100,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new QueryClient();
+
+  Router.events.on("routeChangeStart", progress.start);
+  Router.events.on("routeChangeError", progress.finish);
+  Router.events.on("routeChangeComplete", progress.finish);
 
   return (
     <QueryClientProvider client={client}>
