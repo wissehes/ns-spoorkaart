c5a70c4f5d267b7a951e27c9bb5182a2c9669e4f
Wisse <wissehes@gmail.com>
2022-10-23 20:30:06 +0200

    Add title and format station type

--- pages/stations/[code].tsx
+++ pages/stations/[code].tsx
@@ -7,30 +7,50 @@ import { JourneyDetails, Stop } from "../../types/getJourneyDetailsResponse";
 import { Station } from "../../types/getStationsResponse";
 import { TrainInformation } from "../../types/getTrainInfoResponse";
 import DepartureCard from "../../components/StationPage/DepartureCard";
+import Head from "next/head";
+
+const formatStationType = (type: string) => {
+  switch (type) {
+    case "MEGA_STATION":
+      return "Megastation";
+    case "KNOOPPUNT_STOPTREIN_STATION":
+      return "Stoptrijn knooppunt";
+    case "KNOOPPUNT_INTERCITY_STATION":
+      return "Intercity knooppunt";
+    default:
+      return type;
+  }
+};
 
 export default function StationPage({
   departures,
   station,
-  language,
 }: {
   departures: DepartureWithJourney[];
   // departures:
   station: Station;
-  language?: string;
 }) {
   return (
-    <div className="container">
-      <section className="hero">
-        <div className="hero-body">
-          <p className="title">{station.namen.lang}</p>
-          <p className="subtitle">{station.stationType}</p>
-        </div>
-      </section>
+    <>
+      <Head>
+        <title>{station.namen.lang} vertrektijden | NS Spoorkaart</title>
+      </Head>
 
-      {departures.map((d) => (
-        <DepartureCard departure={d} key={d.departure.product.number} />
-      ))}
-    </div>
+      <div className="container">
+        <section className="hero">
+          <div className="hero-body">
+            <p className="title">{station.namen.lang}</p>
+            <p className="subtitle">
+              Vertrektijden | {formatStationType(station.stationType)}
+            </p>
+          </div>
+        </section>
+
+        {departures.map((d) => (
+          <DepartureCard departure={d} key={d.departure.product.number} />
+        ))}
+      </div>
+    </>
   );
 }
 
@@ -46,7 +66,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     const stations = await getStations();
     const foundStation = stations.payload.find(
-      (s) => s.code == code.toUpperCase()
+      (s) => s.code == code.toUpperCase() || s.UICCode == code
     );
 
     if (!foundStation) {
@@ -55,17 +75,22 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       };
     }
 
-    const departures = await getDepartures(code, 10);
+    const departures = await getDepartures(foundStation.code, 10);
 
     const departuresWithTripInfo: DepartureWithJourney[] = [];
 
     for (const d of departures) {
       // const train = await getTrainInformation(d.product.number);
       const journey = await getJourney(d.product.number);
+
       const foundStop = journey.stops.find(
         (s) => s.stop.uicCode == foundStation.UICCode
       );
-      departuresWithTripInfo.push({ departure: d, stop: foundStop });
+
+      departuresWithTripInfo.push({
+        departure: d,
+        stop: foundStop,
+      });
       // departuresWithTripInfo.push({ departure: d, train });
     }
 
