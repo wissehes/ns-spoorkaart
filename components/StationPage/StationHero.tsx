6e248c586f3b50a3a9f1bb7eddc503014a1b1914
Wisse <wissehes@gmail.com>
2022-10-24 14:15:00 +0200

    Move the StationHero to its own file

---
+++ components/StationPage/StationHero.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import SpoorIcon from "./SpoorIcon";
+
+import { formatStationType } from "../../helpers/StationPage";
+
+import { Station } from "../../types/getStationsResponse";
+
+type TabType = "departures" | "arrivals";
+
+export default function StationHero({
+  station,
+  activeTab,
+}: {
+  station: Station;
+  activeTab: TabType;
+}) {
+  return (
+    <section className="hero is-info" style={{ marginBottom: "2rem" }}>
+      <div className="hero-body">
+        <p className="title">{station.namen.lang}</p>
+        <p className="subtitle">
+          {formatStationType(station.stationType)}
+          {station.synoniemen.length > 0 &&
+            " | Ook bekend als: " + station.synoniemen.join(", ")}
+        </p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            overflow: "scroll",
+          }}
+        >
+          {station.sporen.map((a) => (
+            <SpoorIcon key={a.spoorNummer} spoorNr={a.spoorNummer} />
+          ))}
+        </div>
+      </div>
+
+      <div className="hero-foot">
+        <nav className="tabs is-boxed is-fullwidth">
+          <div className="container">
+            <ul>
+              <li className={activeTab == "departures" ? "is-active" : ""}>
+                <Link href={`/stations/${station.code}/departures`}>
+                  <a>Vertrektijden</a>
+                </Link>
+              </li>
+              <li className={activeTab == "arrivals" ? "is-active" : ""}>
+                <Link href={`/stations/${station.code}/arrivals`}>
+                  <a>Aankomsttijden</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+}
