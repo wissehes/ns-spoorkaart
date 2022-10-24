68e5a9b93e591d985fafb9b2410c7b95d8922b26
Wisse <wissehes@gmail.com>
2022-10-24 14:15:19 +0200

    Move the departures page to its own url

---
+++ pages/stations/[code]/departures.tsx
@@ -0,0 +1,102 @@
+import { GetServerSideProps } from "next";
+import getJourney from "../../../helpers/getJourney";
+import getStations from "../../../helpers/getStations";
+import { getDepartures } from "../../../helpers/stationHelpers";
+import { Departure } from "../../../types/getDeparturesResponse";
+import { JourneyDetails, Stop } from "../../../types/getJourneyDetailsResponse";
+import { Station } from "../../../types/getStationsResponse";
+import { TrainInformation } from "../../../types/getTrainInfoResponse";
+import DepartureCard from "../../../components/StationPage/DepartureCard";
+import Head from "next/head";
+import NavBar from "../../../components/NavBar";
+import SpoorIcon from "../../../components/StationPage/SpoorIcon";
+import Link from "next/link";
+import { formatStationType } from "../../../helpers/StationPage";
+import StationHero from "../../../components/StationPage/StationHero";
+
+export default function DeparturesPage({
+  departures,
+  station,
+}: {
+  departures: DepartureWithJourney[];
+  // departures:
+  station: Station;
+}) {
+  return (
+    <>
+      <Head>
+        <title>{station.namen.lang} vertrektijden | NS Spoorkaart</title>
+      </Head>
+
+      <NavBar />
+
+      <StationHero station={station} activeTab="departures" />
+
+      <div className="container">
+        {departures.map((d) => (
+          <DepartureCard departure={d} key={d.departure.product.number} />
+        ))}
+      </div>
+      <div style={{ padding: "2rem" }} />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const code = context.params?.code;
+
+  if (!code || code instanceof Array) {
+    return {
+      notFound: true,
+    };
+  }
+
+  try {
+    const stations = await getStations();
+    const foundStation = stations.payload.find(
+      (s) => s.code == code.toUpperCase() || s.UICCode == code
+    );
+
+    if (!foundStation) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const departures = await getDepartures(foundStation.code, 10);
+
+    const departuresWithTripInfo: DepartureWithJourney[] = [];
+
+    for (const d of departures) {
+      // const train = await getTrainInformation(d.product.number);
+      const journey = await getJourney(d.product.number);
+
+      const foundStop = journey.stops.find(
+        (s) => s.stop.uicCode == foundStation.UICCode
+      );
+
+      departuresWithTripInfo.push({
+        departure: d,
+        stop: foundStop,
+      });
+    }
+
+    return {
+      props: {
+        departures: departuresWithTripInfo,
+        station: foundStation,
+      },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export interface DepartureWithJourney {
+  departure: Departure;
+  journey?: JourneyDetails;
+  stop?: Stop;
+}
