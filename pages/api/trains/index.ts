6b2c708423a94f085519eb05fe9214f0b617fa70
Wisse <wissehes@gmail.com>
2022-10-30 14:08:17 +0100

    Make the trains api method also fetch additional info for each train, and then combine it

--- pages/api/trains/index.ts
+++ pages/api/trains/index.ts
@@ -2,15 +2,46 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import getTrains from "../../../helpers/getTrains";
+import NS from "../../../helpers/NS";
+import { getMultipleTrainsInfoResponse } from "../../../types/getTrainInfoResponse";
 import { getTrainsResponse, Trein } from "../../../types/getTrainsResponse";
+import { TreinWithInfo } from "../../../types/getTrainsWithInfoResponse";
 
-type Data = Trein[];
+type Data = TreinWithInfo[];
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const data = await getTrains();
+  const trainsInfo = await getTrainInfo(data.payload.treinen);
 
-  res.status(200).json(data.payload.treinen);
+  const treinenMetInfo: TreinWithInfo[] = [];
+
+  for (const trein of data.payload.treinen) {
+    const foundInfo = trainsInfo.find((a) => a.ritnummer == trein.treinNummer);
+    if (foundInfo) {
+      treinenMetInfo.push({
+        ...trein,
+        info: foundInfo,
+      });
+    } else {
+      treinenMetInfo.push({
+        ...trein,
+      });
+    }
+  }
+
+  res.status(200).json(treinenMetInfo);
+}
+
+async function getTrainInfo(trains: Trein[]) {
+  const { data } = await NS.get<getMultipleTrainsInfoResponse>(
+    "/virtual-train-api/api/v1/trein",
+    {
+      params: { ids: trains.map((t) => t.ritId).join(","), all: false },
+    }
+  );
+
+  return data;
 }
