7e04b9c7b7dd7202e4b06534eb6b707f7d3f2c96
Wisse <wissehes@gmail.com>
2022-10-31 17:55:49 +0100

    Add rush icons + refetching of the journey

--- pages/train/[train]/info.tsx
+++ pages/train/[train]/info.tsx
@@ -5,7 +5,7 @@ import { useMemo } from "react";
 // import { GeoJSONObject } from "leaflet";
 import NavBar from "../../../components/NavBar";
 import NS from "../../../helpers/NS";
-import { formatTime } from "../../../helpers/StationPage";
+import { formatDelay, formatTime } from "../../../helpers/StationPage";
 import {
   getJourneyDetailsResponse,
   JourneyDetails,
@@ -17,6 +17,18 @@ import {
   NSGeoJSON,
 } from "../../../types/getGeoJSONResponse";
 import JourneyMap from "../../../components/TrainPage/JourneyMap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTriangleExclamation,
+  faCircle,
+  faCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import useTrains from "../../../hooks/useTrains";
+import { TreinWithInfo } from "../../../types/getTrainsWithInfoResponse";
+import dayjs from "dayjs";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import RushIcon from "../../../components/TrainPage/RushIcon";
 
 const getTrainData = (journey: JourneyDetails) => {
   const stop = journey.stops.find((s) => s.departures[0]);
@@ -24,12 +36,31 @@ const getTrainData = (journey: JourneyDetails) => {
 };
 
 export default function TrainInfoPage({
-  journey,
+  initialJourney,
   geojson,
+  trainId,
 }: {
-  journey: JourneyDetails;
+  initialJourney: JourneyDetails;
   geojson: NSGeoJSON;
+  trainId: string;
 }) {
+  const trains = useTrains();
+  const { data: journey } = useQuery(
+    ["journey", trainId],
+    async () => {
+      const { data } = await axios.get<JourneyDetails>(
+        `/api/trains/${trainId}`
+      );
+      return data;
+    },
+    { initialData: initialJourney /*refetchInterval: 5000*/ }
+  );
+
+  const train = useMemo(
+    () => trains.data?.find((t) => t.ritId == journey.productNumbers[0]),
+    [trains, journey]
+  );
+
   const stops = journey.stops.filter(({ status }) => status !== "PASSING");
   const firstStop = getTrainData(journey);
   const product = firstStop?.departures[0]?.product;
@@ -54,6 +85,8 @@ export default function TrainInfoPage({
           </p>
           <div className="hero-foot">
             <div className="is-flex" style={{ gap: "1rem" }}>
+              <p>Rit {product?.number || journey.productNumbers[0] || "?"}</p>
+              <p>|</p>
               <p>Drukte: {firstStop?.departures[0]?.crowdForecast}</p>
               <p>|</p>
               <p>Zitplaatsen: {firstStop?.actualStock?.numberOfSeats}</p>
@@ -91,22 +124,21 @@ export default function TrainInfoPage({
           </div>
         </div>
       </section>
-
       <div className="container">
+        {!trains.isLoading && !train && <JourneyIsOld />}
+
         <div className="box">
           <h1 className="is-size-3">Route</h1>
-          <div style={{ width: "100%", height: "500px", zIndex: 1 }}>
-            <JourneyMap
-              geojson={geojson}
-              stops={stops}
-              trainId={journey.productNumbers[0]}
-            />
+          <div style={{ width: "100%", height: "300px", zIndex: 1 }}>
+            <JourneyMap geojson={geojson} stops={stops} train={train} />
           </div>
         </div>
 
-        {stops.map((a) => (
-          <TrainCard a={a} key={a.id} />
-        ))}
+        <div className="box">
+          <h1 className="is-size-3">Reis</h1>
+
+          <StopTable stops={stops} train={train} />
+        </div>
       </div>
 
       <div style={{ padding: "2rem" }} />
@@ -118,28 +150,104 @@ const time = (stop: Stop, type: "dest" | "arr") => {
   switch (type) {
     case "arr":
       return stop.arrivals[0]
-        ? formatTime(stop.arrivals[0]?.actualTime)
+        ? formatTime(stop.arrivals[0]?.plannedTime)
         : "--:--";
     case "dest":
       return stop.departures[0]
-        ? formatTime(stop.departures[0]?.actualTime)
+        ? formatTime(stop.departures[0]?.plannedTime)
         : "--:--";
   }
 };
 
-function TrainCard({ a }: { a: Stop }) {
+const delayTime = (stop: Stop, type: "dep" | "arr") => {
+  const delay =
+    type == "dep"
+      ? stop.departures[0]?.delayInSeconds || 0
+      : stop.arrivals[0]?.delayInSeconds || 0;
+
+  if (delay > 30) {
+    return <span className="has-text-danger">+{formatDelay(delay)}</span>;
+  } else {
+    return <></>;
+  }
+};
+
+function StopTable({ stops, train }: { stops: Stop[]; train?: TreinWithInfo }) {
   return (
-    <div className="box is-flex" style={{ gap: "1rem" }}>
-      <div style={{ flexDirection: "column", justifyContent: "space-between" }}>
-        <h1 className="is-size-5 has-text-weight-bold">A: {time(a, "arr")}</h1>
-        <h1 className="is-size-5 has-text-weight-bold">V: {time(a, "dest")}</h1>
-      </div>
+    <table className="table" style={{ width: "100%" }}>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Aankomst</th>
+          <th>Vertrek</th>
+          <th>Station</th>
+          <th>Spoor</th>
+          <th>Drukte</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {stops.map((s) => (
+          <tr
+            key={s.id}
+            className={
+              s.id.replace("_0", "") == train?.info?.station
+                ? "is-selected"
+                : ""
+            }
+          >
+            <th>
+              {checkIfDateHasPassed(
+                s.arrivals[0]?.actualTime || s.departures[0].actualTime
+              ) ? (
+                <FontAwesomeIcon type="regular" icon={faCircleCheck} />
+              ) : (
+                <FontAwesomeIcon type="regular" icon={faCircle} />
+              )}
+            </th>
+            <th>
+              {time(s, "arr")} {delayTime(s, "arr")}
+            </th>
+            <th>
+              {time(s, "dest")} {delayTime(s, "dep")}
+            </th>
+            <th>{s.stop.name}</th>
+            <th>{s.arrivals[0]?.actualTrack || "?"}</th>
+            <th>
+              <RushIcon stop={s} />
+            </th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+const checkIfDateHasPassed = (date: string) => {
+  const parsed = new Date(date);
+  const now = new Date();
+
+  return parsed.getTime() < now.getTime();
+};
+
+function JourneyIsOld() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       <div
-        className="is-flex"
-        style={{ flexDirection: "column", justifyContent: "space-evenly" }}
+        className="notification is-warning"
+        style={{ width: "80%", margin: "1rem" }}
       >
-        <p className="is-size-5">{a.stop.name}</p>
-        <p>{a.status}</p>
+        <span className="icon-text">
+          <span className="icon ">
+            <FontAwesomeIcon icon={faTriangleExclamation} />
+          </span>
+          Deze reis is niet meer live.
+        </span>
       </div>
     </div>
   );
@@ -166,11 +274,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   );
 
-  // console.log(JSON.stringify(geojson.data));
-
   return {
     props: {
-      journey,
+      initialJourney: journey,
+      trainId: train,
       geojson: geojson.data.payload,
     },
   };
