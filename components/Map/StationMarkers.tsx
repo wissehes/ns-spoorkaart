44a0c5cb2d4550fce8f874c7f8fd08a986a6a204
Wisse <wissehes@gmail.com>
2022-11-01 17:54:19 +0100

    Fly to station with a ?station= query parameter

--- components/Map/StationMarkers.tsx
+++ components/Map/StationMarkers.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Marker, Popup, useMap } from "react-map-gl";
 import useStations from "../../hooks/useStations";
 import { SmallStation } from "../../types/getStationsResponse";
@@ -11,6 +12,25 @@ export default function StationMarkers() {
 
   const stationQuery = useStations();
   const { map } = useMap();
+  const router = useRouter();
+
+  useEffect(() => {
+    const query = router.query?.station;
+    if (!query || typeof query !== "string" || !stationQuery.data) return;
+
+    const foundStation = stationQuery.data.find(({ code }) => code == query);
+
+    if (foundStation) {
+      router.push("/trains", undefined, { shallow: true });
+      setStation(foundStation);
+      map?.flyTo({
+        animate: true,
+        duration: 1000,
+        zoom: 15,
+        center: { lat: foundStation.lat, lon: foundStation.lng },
+      });
+    }
+  }, [router, stationQuery, map]);
 
   map?.on("zoomend", () => {
     if (map.getZoom() > 11) {
