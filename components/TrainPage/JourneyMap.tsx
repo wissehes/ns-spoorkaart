1a0e5cfa17f235b8d7b68c7e68074ab6a108514d
Wisse <wissehes@gmail.com>
2022-11-03 22:25:13 +0100

    Add overview and follow train controls

--- components/TrainPage/JourneyMap.tsx
+++ components/TrainPage/JourneyMap.tsx
@@ -2,6 +2,7 @@ import maplibreGl from "maplibre-gl";
 import Map, {
   Layer,
   MapProvider,
+  MapRef,
   Marker,
   NavigationControl,
   Popup,
@@ -13,7 +14,7 @@ import "maplibre-gl/dist/maplibre-gl.css";
 
 import { NSGeoJSON } from "../../types/getGeoJSONResponse";
 import { Stop } from "../../types/getJourneyDetailsResponse";
-import { useEffect, useMemo, useState } from "react";
+import { createRef, useCallback, useEffect, useMemo, useState } from "react";
 import bbox from "@turf/bbox";
 import { time } from "../../helpers/TrainPage";
 import useTrains from "../../hooks/useTrains";
@@ -28,72 +29,141 @@ export default function NewJourneyMap({
   stops: Stop[];
   train?: TreinWithInfo;
 }) {
+  const map = createRef<MapRef>();
   const [popup, setPopup] = useState<Stop | null>(null);
+  const [state, setState] = useState<"overview" | "following" | "custom">(
+    "overview"
+  );
+  const parsed = useMemo(() => bbox(geojson), [geojson]);
+  const speed = useMemo(() => Math.round(train?.snelheid || 0), [train]);
+
+  const fitBounds = useCallback(() => {
+    map.current?.fitBounds(
+      [
+        [parsed[0], parsed[1]],
+        [parsed[2], parsed[3]],
+      ],
+      { padding: { top: 50, left: 40, right: 40, bottom: 20 } }
+    );
+    setState("overview");
+  }, [map, parsed]);
+
+  const followTrain = useCallback(() => {
+    if (!train) return;
+    map.current?.flyTo({
+      zoom: 13,
+      animate: true,
+      center: { lat: train.lat, lon: train.lng },
+    });
+    setState("following");
+  }, [train, map]);
+
+  useEffect(() => {
+    map.current?.on("dragstart", () => setState("custom"));
+  }, [map, fitBounds]);
+
+  useEffect(() => {
+    if (state !== "following" || !train || map.current?.isEasing()) return;
+    map.current?.flyTo({
+      center: { lat: train?.lat, lon: train?.lng },
+      animate: true,
+      duration: 500,
+    });
+  }, [train, map, state]);
 
   return (
-    <MapProvider>
-      <Map
-        id="map"
-        // ref={mapRef}
-        mapLib={maplibreGl}
-        style={{ height: "100%", zIndex: 1, borderRadius: "10px" }}
-        scrollZoom={false}
-        mapStyle="https://api.maptiler.com/maps/ea0be450-77fe-405d-8871-1f29aefe697a/style.json?key=Rs8qUKBURAgmwFrBW6Bj"
-        initialViewState={{
-          longitude: 4.9,
-          latitude: 52.1,
-          zoom: 7,
+    <>
+      <MapProvider>
+        <Map
+          id="map"
+          ref={map}
+          mapLib={maplibreGl}
+          style={{ height: "300px", zIndex: 1, borderRadius: "10px" }}
+          scrollZoom={false}
+          mapStyle="https://api.maptiler.com/maps/ea0be450-77fe-405d-8871-1f29aefe697a/style.json?key=Rs8qUKBURAgmwFrBW6Bj"
+          initialViewState={{
+            longitude: 4.9,
+            latitude: 52.1,
+            zoom: 7,
+          }}
+          mapboxAccessToken=""
+        >
+          <NavigationControl position="top-left" />
+          <ScaleControl />
+
+          <RenderGeo geo={geojson} />
+
+          {stops.map((s) => (
+            <Marker
+              key={s.id}
+              latitude={s.stop.lat}
+              longitude={s.stop.lng}
+              style={{ cursor: "pointer" }}
+              onClick={(e) => {
+                e.originalEvent.stopImmediatePropagation();
+                setPopup(s);
+              }}
+            ></Marker>
+          ))}
+
+          {train && (
+            <Marker
+              longitude={train.lng}
+              latitude={train.lat}
+              anchor="center"
+              style={{ cursor: "pointer" }}
+            >
+              {/*eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={`/api/image/${encodeURIComponent(
+                  train.info?.materieeldelen[0].type || "default"
+                )}`}
+                alt={train.ritId}
+                // height="20px"
+                style={{ height: "30px" }}
+              />
+            </Marker>
+          )}
+
+          {popup && (
+            <Popup
+              longitude={popup.stop.lng}
+              latitude={popup.stop.lat}
+              onClose={() => setPopup(null)}
+            >
+              <StationPopup s={popup} />
+            </Popup>
+          )}
+        </Map>
+      </MapProvider>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          marginTop: "1rem",
+          alignItems: "center",
         }}
-        mapboxAccessToken=""
       >
-        <NavigationControl position="top-left" />
-        <ScaleControl />
-
-        <RenderGeo geo={geojson} />
-
-        {stops.map((s) => (
-          <Marker
-            key={s.id}
-            latitude={s.stop.lat}
-            longitude={s.stop.lng}
-            style={{ cursor: "pointer" }}
-            onClick={(e) => {
-              e.originalEvent.stopImmediatePropagation();
-              setPopup(s);
-            }}
-          ></Marker>
-        ))}
-
+        <button
+          className={`button ${state == "overview" && "is-active"}`}
+          onClick={fitBounds}
+        >
+          Overzicht
+        </button>
         {train && (
-          <Marker
-            longitude={train.lng}
-            latitude={train.lat}
-            anchor="center"
-            style={{ cursor: "pointer" }}
-          >
-            {/*eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={`/api/image/${encodeURIComponent(
-                train.info?.materieeldelen[0].type || "default"
-              )}`}
-              alt={train.ritId}
-              // height="20px"
-              style={{ height: "30px" }}
-            />
-          </Marker>
-        )}
+          <>
+            <button
+              className={`button ${state == "following" && "is-active"}`}
+              onClick={followTrain}
+            >
+              Volg trein
+            </button>
 
-        {popup && (
-          <Popup
-            longitude={popup.stop.lng}
-            latitude={popup.stop.lat}
-            onClose={() => setPopup(null)}
-          >
-            <StationPopup s={popup} />
-          </Popup>
+            <p>Snelheid: {speed} km/h</p>
+          </>
         )}
-      </Map>
-    </MapProvider>
+      </div>
+    </>
   );
 }
 
