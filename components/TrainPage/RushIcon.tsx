9a83f497ce0b18262b14ff49099d54482111117d
Wisse <wissehes@gmail.com>
2022-11-18 22:19:00 +0100

    Add busyness prop to RushIcon component

--- components/TrainPage/RushIcon.tsx
+++ components/TrainPage/RushIcon.tsx
@@ -7,6 +7,8 @@ import Rush1 from "./RushIcons/rush-1.svg";
 import Rush2 from "./RushIcons/rush-2.svg";
 import Rush3 from "./RushIcons/rush-3.svg";
 
+type busyness = "LOW" | "MEDIUM" | "HIGH" | "UNKNOWN" | string;
+
 const icons: { [key: string]: any } = {
   LOW: Rush1,
   MEDIUM: Rush2,
@@ -23,16 +25,18 @@ const colors: { [key: string]: string } = {
 
 export default function RushIcon({
   stop,
+  busyness,
   color = true,
   style,
 }: {
-  stop: Stop;
+  stop?: Stop;
+  busyness?: busyness;
   color?: boolean;
   style?: CSSProperties;
 }) {
-  const busyness = stop.departures[0]?.crowdForecast || "UNKNOWN";
-  const Icon = icons[busyness] || icons.UNKNOWN;
-  const iconColor = color ? colors[busyness] : "white";
+  const _busyness = stop?.departures[0]?.crowdForecast || busyness || "UNKNOWN";
+  const Icon = icons[_busyness] || icons.UNKNOWN;
+  const iconColor = color ? colors[_busyness] : "white";
 
   return (
     <div style={{ ...style, fill: iconColor }}>
