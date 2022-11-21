60a6bba8f607882c71d066c0f1448c0fa6459869
Wisse <wissehes@gmail.com>
2022-11-21 20:37:52 +0100

    Add departures procedure

--- server/routers/routes/stationRouter.ts
+++ server/routers/routes/stationRouter.ts
@@ -7,6 +7,10 @@ import getStations from "../../../helpers/getStations";
 import { SmallStations } from "../../../types/getStationsResponse";
 import { RedisJSON } from "@redis/json/dist/commands";
 import { z } from "zod";
+import { getDepartures } from "../../../helpers/stationHelpers";
+import { DepartureWithJourney } from "../../../types/DepartureWithJourney";
+import getJourney from "../../../helpers/getJourney";
+import { TRPCError } from "@trpc/server";
 
 export const stationRouter = router({
   all: procedure.query(async () => {
@@ -24,6 +28,48 @@ export const stationRouter = router({
         s.namen.lang.toLowerCase().includes(lInput)
     );
   }),
+
+  departures: procedure
+    .input(z.object({ code: z.string() }))
+    .query(async ({ input }) => {
+      const stations = await getStations();
+      const foundStation = stations.payload.find(
+        (s) => s.code == input.code.toUpperCase() || s.UICCode == input.code
+      );
+
+      if (!foundStation) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Station not found.",
+        });
+      }
+
+      const departures = await getDepartures(input.code, 10);
+
+      const departuresWithTripInfo: DepartureWithJourney[] = [];
+
+      for (const d of departures) {
+        try {
+          const journey = await getJourney(d.product.number);
+
+          const foundStop = journey.stops.find(
+            (s) => s.stop.uicCode == foundStation.UICCode
+          );
+
+          departuresWithTripInfo.push({
+            departure: d,
+            stop: foundStop,
+          });
+        } catch (e) {
+          console.log(`[TRAINS] Fetching journey ${d.product.number} failed.`);
+          departuresWithTripInfo.push({
+            departure: d,
+          });
+        }
+      }
+
+      return departuresWithTripInfo;
+    }),
 });
 
 async function getStationsFromDB() {
