0809d6bf5c83772ca2099b5adbf928b8dc7d871c
Wisse <wissehes@gmail.com>
2022-12-24 23:36:59 +0100

    Add trainNumber query

--- server/routers/routes/journeyRouter.ts
+++ server/routers/routes/journeyRouter.ts
@@ -1,3 +1,5 @@
+import { TRPCError } from "@trpc/server";
+import { AxiosError } from "axios";
 import { z } from "zod";
 import NS from "../../../helpers/NS";
 import { getJourneyDetailsResponse } from "../../../types/getJourneyDetailsResponse";
@@ -28,6 +30,28 @@ export const journeyRouter = router({
       );
       return data.payload;
     }),
+  trainNumber: procedure.input(z.string()).query(async ({ input }) => {
+    try {
+      const { data } = await NS.get<number>(
+        `/virtual-train-api/api/v1/ritnummer/${input}`
+      );
+
+      const { data: journey } = await NS.get<getJourneyDetailsResponse>(
+        "/reisinformatie-api/api/v2/journey",
+        {
+          params: { train: data },
+        }
+      );
+
+      return journey.payload;
+    } catch (e) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Geen trein gevonden met dat nummer.",
+        cause: e,
+      });
+    }
+  }),
 });
 
 async function planTrip({
