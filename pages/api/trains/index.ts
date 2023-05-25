e5e786d428e80951630d52b727a0009572254e8a
Wisse <wissehes@gmail.com>
2023-05-25 23:05:51 +0200

    remove unnecessary stuff

--- pages/api/trains/index.ts
+++ pages/api/trains/index.ts
@@ -1,16 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import getTrains from "../../../helpers/getTrains";
-import NS from "../../../helpers/NS";
-import { getMultipleTrainsInfoResponse } from "../../../types/getTrainInfoResponse";
-import { Trein } from "../../../types/getTrainsResponse";
 import { TreinWithInfo } from "../../../types/getTrainsWithInfoResponse";
 
-import Jimp from "jimp";
-// @ts-ignore
-import replaceColor from "replace-color";
 import DB from "../../../lib/DB";
+import { getTrainInfo } from "../../../helpers/trains/getTrainInfo";
+import { downloadAndSaveImage } from "../../../helpers/trains/downloadAndSaveImage";
+import { saveTrains } from "../../../helpers/trains/saveTrains";
 
 type Data = TreinWithInfo[];
 
@@ -38,71 +34,8 @@ export default async function handler(
   }
 
   await downloadAndSaveImage(treinenMetInfo);
-  await DB.saveTrains(treinenMetInfo);
+  // await DB.saveTrains(treinenMetInfo);
+  await saveTrains(treinenMetInfo);
 
   res.status(200).json(treinenMetInfo);
-}
-
-/**
- * This mechanism first filters all trains, to make sure the material parts exist,
- * then we check if the images already exist. If they don't, then we're looping over
- * every train and download, resize and remove the background of every image. Lastly,
- * we save the image (usually around 3kb) to Redis.
- */
-
-async function downloadAndSaveImage(trains: TreinWithInfo[]) {
-  const exists = await DB.imageArrayExists();
-  if (exists) {
-    return;
-  }
-
-  for (const train of trains) {
-    if (!train.info) continue;
-
-    const mat = train.info.materieeldelen[0];
-    const url = mat?.bakken[0]?.afbeelding?.url;
-    if (!mat || !url) continue;
-
-    const exists = await DB.trainImgExists(mat.type);
-    if (exists) {
-      continue;
-    }
-
-    const img = (await Jimp.read(url))
-      .resize(Jimp.AUTO, 50)
-      .crop(0, 0, 100, 50);
-    if (img.hasAlpha()) {
-      img.rgba(true).background(0x000000ff);
-    }
-
-    const imgBuffer = await img.getBufferAsync(Jimp.MIME_PNG);
-    const imgReplaced = await replaceColor({
-      image: imgBuffer,
-      colors: {
-        type: "hex",
-        targetColor: "#ffffff",
-        replaceColor: "#00000000",
-      },
-    });
-
-    const imgReplacedBuffer = await imgReplaced.getBufferAsync(Jimp.MIME_PNG);
-    const base64 = Buffer.from(imgReplacedBuffer, "binary").toString("base64");
-
-    await DB.saveTrainImg({ type: mat.type, base64data: base64 });
-  }
-}
-
-async function getTrainInfo(trains: Trein[]) {
-  const { data } = await NS.get<getMultipleTrainsInfoResponse>(
-    "/virtual-train-api/api/v1/trein",
-    {
-      params: {
-        features: "zitplaats",
-        ids: trains.map((t) => t.ritId).join(","),
-        all: false,
-      },
-    }
-  );
-
-  return data;
 }
