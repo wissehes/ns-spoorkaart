c4a139f5d0cf73614cf93a2c25b42f048cbddd16
Wisse <wissehes@gmail.com>
2023-05-27 15:22:06 +0200

    update prisma log level

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,7 +11,7 @@ const globalForPrisma = global as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log: ["info"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
