616b26f48e5d52b8887a9f8c0d5d6b8027ff7bcb
Wisse <wissehes@gmail.com>
2023-06-01 17:02:04 +0200

    Split positions up into journeys to fix the straight lines across the map

--- hooks/usePositonsGeojson.ts
+++ hooks/usePositonsGeojson.ts
@@ -6,18 +6,45 @@ import { useMemo } from "react";
  * @param positions An array of positions from Prisma
  */
 export default function usePositionsGeojson(positions: TrainPosition[]) {
+  /**
+   * Splits all positions up into their journey's.
+   * This avoids the random straight lines across the map.
+   */
+  const journeys = useMemo(() => {
+    const journeys: Map<string, { lat: number; lng: number }[]> = new Map();
+
+    for (const position of positions) {
+      const id = position.journeyId;
+      const newLoc = { lat: position.lat, lng: position.lng };
+      const exists = journeys.get(id);
+
+      if (exists) {
+        journeys.set(id, [...exists, newLoc]);
+      } else {
+        journeys.set(id, [newLoc]);
+      }
+    }
+
+    return Array.from(journeys, ([key, value]) => ({
+      id: key,
+      locations: value,
+    }));
+  }, [positions]);
+
   const geojson = useMemo(
     () => ({
       type: "FeatureCollection",
       features: [
-        {
+        ...journeys.map((loc) => ({
           type: "Feature",
           properties: {},
           geometry: {
             type: "LineString",
-            coordinates: positions.map((d) => [d.lng, d.lat]),
+            id: loc.id,
+            coordinates: loc.locations.map((d) => [d.lng, d.lat]),
           },
-        },
+        })),
+
         ...positions.map((d) => ({
           type: "Feature",
           geometry: {
@@ -27,7 +54,7 @@ export default function usePositionsGeojson(positions: TrainPosition[]) {
         })),
       ],
     }),
-    [positions]
+    [positions, journeys]
   );
   return geojson;
 }
