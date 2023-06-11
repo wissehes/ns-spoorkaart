df7055ed209be7b942d48374446580070b95e889
Wisse <wissehes@gmail.com>
2023-06-11 13:42:20 +0200

    Only refetch if it is enabled

--- hooks/useTrains.ts
+++ hooks/useTrains.ts
@@ -1,15 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Trein } from "../types/getTrainsResponse";
 import { TreinWithInfo } from "../types/getTrainsWithInfoResponse";
+import { useMapStore } from "../stores/MapStore";
 
-export default function useTrains(refresh: Boolean = true) {
+export default function useTrains() {
+  const shouldRefresh = useMapStore((s) => s.autoRefresh);
+
   return useQuery(
     ["trains"],
     async () => {
       const { data } = await axios.get<TreinWithInfo[]>("/api/trains");
       return data;
     },
-    { refetchInterval: refresh ? 4000 : false }
+    { refetchInterval: shouldRefresh ? 4000 : false }
   );
 }
