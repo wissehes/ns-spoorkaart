b938ed55d4c5690242852327ffd1a2670fbb7f9f
Wisse <wissehes@gmail.com>
2023-06-11 13:42:12 +0200

    Add autorefresh and persistance to the store

--- stores/MapStore.ts
+++ stores/MapStore.ts
@@ -1,17 +1,27 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export interface MapState {
   shouldRotate: boolean;
   showTrack: boolean;
+  autoRefresh: boolean;
 
   setRotate: (to: boolean) => void;
   setShowTrack: (to: boolean) => void;
+  setAutoRefresh: (to: boolean) => void;
 }
 
-export const useMapStore = create<MapState>((set) => ({
-  shouldRotate: true,
-  showTrack: true,
+export const useMapStore = create<MapState>()(
+  persist(
+    (set) => ({
+      shouldRotate: true,
+      showTrack: true,
+      autoRefresh: true,
 
-  setRotate: (v) => set({ shouldRotate: v }),
-  setShowTrack: (v) => set({ showTrack: v }),
-}));
+      setRotate: (v) => set({ shouldRotate: v }),
+      setShowTrack: (v) => set({ showTrack: v }),
+      setAutoRefresh: (v) => set({ autoRefresh: v }),
+    }),
+    { name: "map-store", storage: createJSONStorage(() => localStorage) }
+  )
+);
